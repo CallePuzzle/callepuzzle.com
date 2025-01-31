5cb8c4b2ca93af76ccfe1b00fb8461f74ecac985
César M. Cristóbal <cesar@callepuzzle.com>
2025-01-31 21:06:41 +0100

    gradient of logo text

--- src/lib/components/layout/Nav.svelte
+++ src/lib/components/layout/Nav.svelte
@@ -2,7 +2,7 @@
 	import { Icon } from 'svelte-icons-pack';
 	import { BiMenu } from 'svelte-icons-pack/bi';
 	import logo from '$lib/assets/logo.png';
-  import NavList from '$lib/components/layout/NavList.svelte';
+	import NavList from '$lib/components/layout/NavList.svelte';
 </script>
 
 <nav class="navbar bg-base-100 shadow-sm">
@@ -11,17 +11,26 @@
 			<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
 				<Icon src={BiMenu} size="32" />
 			</div>
-      <NavList isBurgerMenu={true} />
+			<NavList isBurgerMenu={true} />
 		</div>
 		<a href="/" class="btn btn-ghost text-xl">
 			<img src={logo} alt="Logo" class="h-8 w-auto" />
-      <span>Calle Puzzle</span>
+			<span class="calle-puzzle-gradient">Calle Puzzle</span>
 		</a>
 	</div>
 	<div class="navbar-center hidden lg:flex">
-    <NavList isBurgerMenu={false} />
+		<NavList isBurgerMenu={false} />
 	</div>
 	<div class="navbar-end">
 		<a class="btn">Button</a>
 	</div>
 </nav>
+
+<style>
+	.calle-puzzle-gradient {
+		background: linear-gradient(90deg, #0884ba, #21a1d7, #17a3d4);
+		-webkit-background-clip: text;
+		background-clip: text;
+		color: transparent;
+	}
+</style>
