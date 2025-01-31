79c384bd94e34e534563f037f4f646fe4f7e11ca
César M. Cristóbal <cesar@callepuzzle.com>
2025-01-31 21:29:34 +0100

    1 header (#2)
    * upgrade and add basic nav
    
    * wip nav
    
    * gradient of logo text
    
    * add github icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	import { i18n } from "$lib/i18n";
-	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
+	import Nav from '$lib/components/layout/Nav.svelte';
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
+	<Nav />
 	{@render children()}
 </ParaglideJS>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { paraglide } from "@inlang/paraglide-sveltekit/vite";
-import { defineConfig } from "vitest/config";
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [tailwindcss(), sveltekit(), paraglide({
-        project: "./project.inlang",
-        outdir: "./src/lib/paraglide"
-    })],
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
+	],
 
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}']
-    }
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 });
