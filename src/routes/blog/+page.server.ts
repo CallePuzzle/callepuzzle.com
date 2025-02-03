9a6f7d0b69974fb62967dd065a6696536f8843bf
César M. Cristóbal <cesar@callepuzzle.com>
2025-02-03 13:28:01 +0100

    log environment

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -4,6 +4,9 @@ import FusionCollection from 'fusionable/FusionCollection';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
+
+	logger.warn(dev, 'environment');
+
 	const buildDir = dev ? '' : '/.svelte-kit/output/server';
 	const postDataDir = process.cwd() + buildDir + '/src/data-posts/2020-05';
 	const collection = new FusionCollection().loadFromDir(postDataDir).orderBy('date', 'desc');
