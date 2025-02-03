2f9987241cc92c453a487928896b5cfa4f240b82
César M. Cristóbal <cesar@callepuzzle.com>
2025-02-03 18:05:52 +0100

    another way

--- src/routes/blog/[...slug]/+page.svelte
+++ src/routes/blog/[...slug]/+page.svelte
@@ -7,16 +7,16 @@
 		data: PageData;
 	} = $props();
 
-	const { PostContent, post } = data;
+	const { content: PostContent, post, metadata } = data;
 </script>
 
-<div class="hero w-screen" style={post.image ? `background-image: url(${post.image});` : ''}>
+<div class="hero w-screen" style={metadata.image ? `background-image: url(${metadata.image});` : ''}>
 	<div class="hero-overlay"></div>
 	<div class="hero-content text-neutral-content text-center">
 		<div class="max-w-md">
-			<h1 class="mb-5 text-5xl font-bold">{post.title}</h1>
+			<h1 class="mb-5 text-5xl font-bold">{metadata.title}</h1>
 			<p class="mb-5">
-				{post.description}
+				{metadata.description}
 			</p>
 		</div>
 	</div>
--- src/routes/blog/[...slug]/+page.ts
+++ src/routes/blog/[...slug]/+page.ts
@@ -1,15 +1,47 @@
-import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
+import type { EntryGenerator, PageLoad } from './$types';
 
-export const load: PageLoad = async ({ data }) => {
-	// load the markdown file based on slug
-	/* @vite-ignore */
-	const postContent = await import(data.post.filepath);
+import type { Component } from 'svelte';
+
+export type ContentMetadata = {
+	title: string;
+	description: string;
+};
+
+export type ContentModules = Record<
+	string,
+	() => Promise<{ default: Component; metadata: ContentMetadata }>
+>;
+
+export const load: PageLoad = async ({ params, data }) => {
+	const posts = import.meta.glob<GlobEntry>('/src/data-posts/**/*.md', {
+		eager: true
+	}) as ContentModules;
+
+	const post = posts['/src/data-posts/' + params.slug + '.md'];
+	console.log('slug', params.slug);
+	console.log('posts', posts);
+	if (!post) {
+		throw error(404, 'Post not found');
+	}
 
 	return {
-		post: data.post,
-		PostContent: postContent.default,
-		layout: {
-			fullWidth: true
-		}
+		post,
+		content: post.default,
+		metadata: post.metadata
 	};
 };
+
+export const entries: EntryGenerator = async () => {
+	const posts = import.meta.glob<GlobEntry>('/src/data-posts/**/*.md', {
+		eager: true
+	}) as ContentModules;
+
+	const entries = Object.keys(posts).map((path) => {
+		return { slug: path.replace('.md', '').replace('/src/data-posts/', '') };
+	});
+
+	return entries;
+};
+
+export const prerender = true;
