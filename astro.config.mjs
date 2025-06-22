a3fe1285ec3d9499caa62da85d4b81eb2b18e9c0
César M. Cristóbal <cesar@callepuzzle.com>
2025-06-22 13:09:50 +0200

    index

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,18 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
 });
