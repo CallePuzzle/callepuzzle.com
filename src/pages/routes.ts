887da546f4f02b87502dc14201861213ba2086b5
César M. Cristóbal <cesar@callepuzzle.com>
2025-06-22 14:03:09 +0200

    migrate header

---
+++ src/pages/routes.ts
@@ -0,0 +1,25 @@
+interface Route {
+  name: string;
+  getUrl: (params?: any) => string;
+}
+
+interface Routes {
+  [id: string]: Route;
+}
+
+const Routes: Routes = {
+  home: {
+    name: "Inicio",
+    getUrl: () => "/",
+  },
+  blog: {
+    name: "Blog",
+    getUrl: () => "/blog",
+  },
+  quienesSomos: {
+    name: "Quienes somos",
+    getUrl: () => "/#quienes-somos",
+  },
+};
+
+export default Routes;
