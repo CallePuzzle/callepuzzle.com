da39896350bcc11f45b50dd77576663bbaaa0fec
César M. Cristóbal <cesar@callepuzzle.com>
2025-06-24 16:37:31 +0200

    Astro (#19)
    * initial
    
    * remove sveltekit
    
    * index
    
    * migrate header
    
    * unused imports
    
    * wip themes
    
    * fix theme swapper
    
    * fix swap theme types
    
    * about us
    
    * add blogs
    
    * fix simple icon makes astro slow
    
    * blog detail

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import vercel from "@astrojs/vercel";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
--- src/routes/+layout.server.ts
+++
@@ -1,6 +0,0 @@
-import type { LayoutServerLoad, LayoutServerLoadEvent } from './$types';
-
-export const load: LayoutServerLoad = async (event: LayoutServerLoadEvent) => {
-	const path = event.route.id;
-	return { path };
-};
