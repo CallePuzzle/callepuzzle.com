43bd5cc79796f53bbffd0b4d8ff59c34c7d16aed
César M. Cristóbal <cesar@callepuzzle.com>
2025-06-24 16:23:33 +0200

    fix simple icon makes astro slow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
